fffe21d36259683fc3ebe11de6282600631d1cc5
Reva Moolky <>
2024-04-15 13:46:10 -0700

    add get user id to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -8,6 +8,8 @@ const findUserByName = (name) => {
         (user) => user["name"] === name
     );
     };
+const findUserById = (id) =>
+users["users_list"].find((user) => user["id"] === id);
 const users = {
     users_list: [
       {
@@ -61,4 +63,12 @@ app.get("/users", (req, res) => {
       res.send(users);
     }
   });
-
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
