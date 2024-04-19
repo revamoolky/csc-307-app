43e798721041832c8450004f54eb7860d7d7635c
Reva Moolky <>
2024-04-19 13:54:04 -0700

    modified table to tadd id

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -17,6 +17,7 @@ function TableHeader() {
     return (
       <thead>
         <tr>
+          <th>id</th>
           <th>Name</th>
           <th>Job</th>
         </tr>
@@ -28,6 +29,7 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
+            <td>{row.id}</td>
             <td>{row.name}</td>
             <td>{row.job}</td>
             <td>
