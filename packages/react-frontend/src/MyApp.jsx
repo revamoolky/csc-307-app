2f82900ecaedc08426d70c82de91448ef7dead5b
Reva Moolky <>
2024-05-06 13:42:31 -0700

    my app

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -13,19 +13,15 @@ function MyApp() {
     //     setCharacters(updated);
     // }
     function removeOneCharacter(index) {
-        const characterId = characters[index].id; // Get the ID of the character to be deleted
-        fetch(`http://localhost:8000/users/${characterId}`, { method: 'DELETE' }) // Make DELETE request to backend
+        const characterId = characters[index]._id;
+        fetch(`http://localhost:8000/users/${characterId}`, { method: 'DELETE' })
             .then((res) => {
                 if (res.status === 204) {
                     const updated = characters.filter((character, i) => {
-                              return i !== index;
-                            });
-                            setCharacters(updated);
-                } else if (res.status === 404) {
-                    // If resource not found, log an error message
-                    console.error('Resource not found. User not deleted.');
+                        return i !== index;
+                    });
+                    setCharacters(updated); // Update state after successful deletion
                 } else {
-                    // For other status codes, log an error message
                     console.error('Error deleting user.');
                 }
             })
@@ -33,51 +29,21 @@ function MyApp() {
                 console.error('Error deleting user:', error);
             });
     }
+    
     function updateList(person) { 
         postUser(person)
             .then((response) => response.json())
             .then((data) => {
-                setCharacters([...characters, { ...person, id: data.id }]); // Include the returned ID in the new user object
+                const newUser = { ...person, _id: data._id }; // Use the received ID from the response
+                setCharacters([...characters, newUser]); // Update state with the new user object
             })
             .catch((error) => {
                 console.error("Error adding user:", error);
             });
     }
     
-    // function updateList(person) { 
-    //     postUser(person)
-    //         .then(() => setCharacters([...characters, person]))
-    //         .catch((error) => {
-    //             console.log(error);
-    //         })
-    // }
-    // function updateList(person) { 
-    //     postUser(person)
-    //       .then((response) => {
-    //         if (response.status === 201) {
-    //           setCharacters([...characters, person]);
-    //         } else {
-    //           console.error("Failed to add user:", response.statusText);
-    //         }
-    //       })
-    //       .catch((error) => {
-    //         console.error("Error adding user:", error);
-    //       });
-    //   }
-    // function updateList(person) { 
-    //     postUser(person)
-    //         .then(() => {
-    //             setCharacters([...characters, person]);
-    //             // Fetch updated user list after adding a new user
-    //             fetchUsers()
-    //                 .then((res) => res.json())
-    //                 .then((json) => setCharacters(json["users_list"]))
-    //                 .catch((error) => { console.error(error); });
-    //         })
-    //         .catch((error) => {
-    //             console.error("Error adding user:", error);
-    //         });
-    // }
+    
+    
     
     
     function fetchUsers() {
