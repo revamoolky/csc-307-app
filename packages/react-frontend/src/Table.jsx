45caf30af3c07fe98ed5e0344e1806182df020e2
Reva Moolky <>
2024-05-06 13:42:48 -0700

    table

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -25,27 +25,44 @@ function TableHeader() {
     );
   }
   
+// function TableBody(props) {
+//     const rows = props.characterData.map((row, index) => {
+//         return (
+//             <tr key={index}>
+//             <td>{row.id}</td>
+//             <td>{row.name}</td>
+//             <td>{row.job}</td>
+//             <td>
+//               {/* <button onClick={() => props.removeCharacter(index)}>
+//                 Delete
+//               </button> */}
+//               <button onClick={() => props.removeCharacter(row._id)}>Delete</button>
+//             </td>
+//           </tr>
+//         );
+//        }
+//       );
+//       return (
+//           <tbody>
+//             {rows}
+//            </tbody>
+//        );
+// }
+
 function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-            <td>{row.id}</td>
-            <td>{row.name}</td>
-            <td>{row.job}</td>
-            <td>
-              <button onClick={() => props.removeCharacter(index)}>
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-       }
-      );
-      return (
-          <tbody>
-            {rows}
-           </tbody>
-       );
+  const rows = props.characterData.map((row) => (
+    <tr key={row._id}>
+      <td>{row._id}</td>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+      <button onClick={() => props.removeCharacter(row._id)}>
+                Delete</button>
+      </td>
+    </tr>
+  ));
+
+  return <tbody>{rows}</tbody>;
 }
 
 export default Table;
